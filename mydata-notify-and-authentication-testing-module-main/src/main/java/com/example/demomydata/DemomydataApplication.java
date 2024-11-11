bb2ff285d14349e47633c317e0158d92c738f288
TsubasaRush <103172464+TsubasaRush@users.noreply.github.com>
2024-11-11 10:51:41 +0800

    Add files via upload

---
+++ mydata-notify-and-authentication-testing-module-main/src/main/java/com/example/demomydata/DemomydataApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomydata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomydataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemomydataApplication.class, args);
+	}
+
+}
